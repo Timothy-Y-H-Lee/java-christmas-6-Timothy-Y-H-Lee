76c48cec22ba1d3c1cc3f333aec75e7c5f9d68b4
Timothy <timothy@jungnanum.com>
2023-11-10 21:40:41 +0900

    feat(#1): 판매 메뉴 정의 #1
    - 메뉴, 가격에 대한 Enum의 키값에 카테고리가 포함되도록 수정

--- src/main/java/enums/MenuNameDefine.java
+++ src/main/java/enums/MenuNameDefine.java
@@ -1,18 +1,18 @@
 package enums;
 
 public enum MenuNameDefine {
-    MENU_MUSHROOM_SOUP("양송이수프"),
-    MENU_TAPAS("타파스"),
-    MENU_CAESAR_SALAD("시저샐러드"),
-    MENU_T_BONE_STEAK("티본스테이크"),
-    MENU_BBQ_RIBS("바비큐립"),
-    MENU_SEAFOOD_PASTA("해산물파스타"),
-    MENU_CHRISTMAS_PASTA("크리스마스파스타"),
-    MENU_CHOCOLATE_CAKE("초코케이크"),
-    MENU_ICE_CREAM("아이스크림"),
-    MENU_ZERO_COKE("제로콜라"),
-    MENU_RED_WINE("레드와인"),
-    MENU_CHAMPAGNE("샴페인")
+    MENU_APPETIZER_MUSHROOM_SOUP("양송이수프"),
+    MENU_APPETIZER_TAPAS("타파스"),
+    MENU_APPETIZER_CAESAR_SALAD("시저샐러드"),
+    MENU_MAIN_T_BONE_STEAK("티본스테이크"),
+    MENU_MAIN_BBQ_RIBS("바비큐립"),
+    MENU_MAIN_SEAFOOD_PASTA("해산물파스타"),
+    MENU_MAIN_CHRISTMAS_PASTA("크리스마스파스타"),
+    MENU_DESSERT_CHOCOLATE_CAKE("초코케이크"),
+    MENU_DESSERT_ICE_CREAM("아이스크림"),
+    MENU_DRINK_ZERO_COKE("제로콜라"),
+    MENU_DRINK_RED_WINE("레드와인"),
+    MENU_DRINK_CHAMPAGNE("샴페인")
     ;
 
     private String value;
