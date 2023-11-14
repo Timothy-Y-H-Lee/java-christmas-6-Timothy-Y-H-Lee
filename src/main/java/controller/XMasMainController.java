e6c9e748868ffc0914914c6764241150afc895da
Timothy <timothy@jungnanum.com>
2023-11-15 03:24:16 +0900

    feat(#3): 할인 방법 정의
    - 전달받은 일자를 이용해서, 크리스마스 디데이 할인이 맞는지 확인

--- src/main/java/controller/XMasMainController.java
+++ src/main/java/controller/XMasMainController.java
@@ -3,15 +3,34 @@ package controller;
 import static enums.EventDateRule.EVENT_MONTH;
 import static enums.UserInterface.INVALIDATE_INPUT_VISIT_DAY;
 
+import domain.VisitDate;
 import java.time.MonthDay;
 import view.InputView;
 
 public class XMasMainController {
     private InputView inputView = new InputView();
     private String visitDay;
+    // 크리스마스 디데이 할인 여부
+    private Boolean isXMasDayDiscount = false;
+    // 평일 할인 여부
+    private Boolean isWeekdaysDiscount = false;
+    // 주말 할인 여부
+    private Boolean isWeekendDiscount = false;
+    // 특별 할인 여부
+    private Boolean isSpecialDiscount = false;
+
 
     public void startXmasEvent() {
         readDate();
+        checkDiscountDays();
+    }
+
+    private void checkDiscountDays() {
+        this.isXMasDayDiscount();
+    }
+
+    private void isXMasDayDiscount() {
+        isXMasDayDiscount = VisitDate.getInstance().rangeIn25Day(visitDay);
     }
 
     public void readDate() {
